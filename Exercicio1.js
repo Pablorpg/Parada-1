38da831ef59e4060a215b6824a4569937ce161af
Pablo <pablogomesph55@gmail.com>
2025-02-03 15:06:03 -0300

    Add files via upload

---
+++ Exercicio1.js
@@ -0,0 +1,11 @@
+// let numero = Number(prompt("Digite um número:"))
+let numero = 6
+
+const verificaNum = (num) => {
+    if (num > 0) {
+        return 1
+    } else {
+        return 0
+    }
+}
+console.log(verificaNum(numero))
